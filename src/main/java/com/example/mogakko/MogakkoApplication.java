4885395b0ff221770af6365ec47f412ebcbec35f
xxhyxxnjxn <wjdguswls55@gmail.com>
2024-07-10 20:33:55 +0900

    project setting

---
+++ src/main/java/com/example/mogakko/MogakkoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mogakko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MogakkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MogakkoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mogakko/MogakkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mogakko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MogakkoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
